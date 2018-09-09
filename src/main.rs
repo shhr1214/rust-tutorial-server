a221fcc82cd99f34a21225f7c21b449149450b2a
shhr1214 <souhei0079@gmail.com>
2018-09-10 00:22:31 +0900

    add: hello.html を読み込んでレスポンスとして返せるように

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -16,7 +17,12 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\n";
+    let mut file = File::open("hello.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\n{}", contents);
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
